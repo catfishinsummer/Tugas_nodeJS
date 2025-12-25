437e416268984d6081ef2e12e164a9e93c68840e
dandika <dandiadriandopo@gmail.com>
2025-12-25 17:02:54 +0700

    sikat

--- data.js
+++ data.js
@@ -1,7 +1,11 @@
-export const menu = [
+const menu = [
     {id: 1, kode: "NG", nama_menu: "Nasi Goreng", harga: 15000},
     {id: 2, kode: "AG", nama_menu: "Nasi Ayam Geprek", harga: 10000},
     {id: 3, kode: "AB", nama_menu: "Nasi Ayam Bakar", harga: 12000},
     {id: 4, kode: "ET", nama_menu: "Es Teh Manis", harga: 5000},
     {id: 5, kode: "EJ", nama_menu: "Es Jeruk Peras", harga: 8000},
 ]
+
+const pesenan = []
+
+module.exports = {menu, pesenan}
